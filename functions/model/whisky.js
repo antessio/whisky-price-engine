b140c405b396ea3bdf3cc3f01f648fc97688fc44
Antonio D'Alessio <antonio.dalessio@satispay.com>
2021-12-15 00:00:14 +0100

    added firebase and implemented whisky shop search

---
+++ functions/model/whisky.js
@@ -0,0 +1,9 @@
+module.exports=class Whisky{
+
+    constructor(productId,distillery,name,price){
+        this.productId = productId
+        this.distillery = distillery
+        this.name = name
+        this.price = price
+    }
+}

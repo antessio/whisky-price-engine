ce5e15a2e9191507a9a77125bd2938dab7aad208
Antonio D'Alessio <antonio.dalessio@satispay.com>
2021-12-16 23:15:30 +0100

    refactoring typescript

--- functions/model/whisky.js
+++
@@ -1,9 +0,0 @@
-module.exports=class Whisky{
-
-    constructor(productId,distillery,name,price){
-        this.productId = productId
-        this.distillery = distillery
-        this.name = name
-        this.price = price
-    }
-}
